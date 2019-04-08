e17487cbc57aee6290a9c48db1b233baa5096dcd
nrlfrthh <48424752+nrlfrthh@users.noreply.github.com>
2019-04-08 21:08:48 +0800

    Update Client.c

--- Client.c
+++ Client.c
@@ -1,44 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
-#include <netdb.h>
-#include <unistd.h>
 #include <arpa/inet.h>
-#define PORT 444
-
-int main(int argc, char const *argv[])
+#include <unistd.h>
+#include<string.h>
+#define PORT 4444
+int main()
 {
-    struct sockaddr_in address;
-    int sockfd = 0, vread;
-    struct sockaddr_in servaddr;
-    char *annyeong = "annyeong from client";
-    char buff[1111] = {0};
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error..try again \n");
-        return -1;
-    }
-    memset(&servaddr, '0', sizeof(servaddr));
-    
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    
-    //convet IPv4 AND IPv6 address from text to binary form
-    if (inet_pton(AF_INET, "192.168.216.128", &servaddr.sin_addr) <=0)
-    {
-        printf("\n Heyy! Your address is invalid \n");
-        return -1;
-    }
-    
-    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) <0)
-    {
-        printf("\n Your connection failed!!!! \n");
-        return -1;
-    }
-    send(sockfd, annyeong , strlen(annyeong) , 0);
-    printf("annyeong my bae\n");
-    vread = read(sockfd , buff , 1111);
-    printf("%s\n",buff);
-    return 0;
+     printf("this is a client program\n");
+     struct sockaddr_in myaddr,clientaddr;
+     int newsockid;
+     int len;
+     int sockid,client_add;
+     sockid=socket(AF_INET,SOCK_STREAM,0);
+     if(sockid==-1)
+     perror("socket");
+     memset(&myaddr,0,sizeof myaddr);
+     myaddr.sin_port=htons(4444);
+     myaddr.sin_family=AF_INET;
+     myaddr.sin_addr.s_addr=inet_addr("192.168.216.128");
+     len=sizeof myaddr;
+      if((bind(sockid,(struct sockaddr*)&myaddr,sizeof myaddr))==-1)
+    perror("bind");
+     int p=connect(sockid,(struct sockaddr*)&myaddr,len);
+     if(p==-1)
+     perror("connect");
+     char msg[200];
+     int i=0;
+     char c;int l=0; char *buffer=(char *)(malloc(sizeof(char)*200)); int buffsize;
+     while(i<10)
+     {l=0;
+                printf("Client: ");
+                while((c=getchar())!='\n')
+                {
+            msg[l++]=c;
+                }
+                msg[l]='\0'; l++;
+                send(sockid,msg,l,0);
+                fflush(stdin);
+                newsockid=accept(sockid,(struct sockaddr*)&clientaddr,&client_add);
+                recv(newsockid,buffer,buffsize,0);
+                   l=0;
+                 fprintf(stdout, "server: %s", buffer);
+                  printf("\n");
+                i++;
+     }
+     close(sockid);
+     return 0;
 }
